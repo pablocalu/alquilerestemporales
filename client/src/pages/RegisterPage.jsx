b3a13bd0218161b4db273d8d92c819204a0639dd
Pablo Carro <pablo.carro@live.com>
2023-02-20 17:30:58 -0300

    backstart

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import Toastify from 'toastify-js'
 
 export default function RegisterPage() {
 
@@ -7,11 +9,50 @@ const [name, setName] = useState('')
 const [email, setEmail] = useState('')
 const [password, setPassword] = useState('')
 
+const registerUser = async (e) => {
+    e.preventDefault()
+
+    try {
+        await axios.post('/register',{
+            name,
+            email,
+            password
+        })
+        Toastify({
+            text: 'User succesfully created. Please Login.',
+            duration: 3000,
+            close: true,
+            gravity: top,
+            newWindow: true,
+            stopOnFocus: true,
+            position: 'center',
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+              },
+        }).showToast()
+    } catch (error) {
+        Toastify({
+            text: 'Registration fail. Try again',
+            duration: 3000,
+            close: true,
+            gravity: top,
+            newWindow: true,
+            stopOnFocus: true,
+            position: 'center',
+            style: {
+                background: "linear-gradient(to right, #ff0000 , #ff5a3b)",
+              },
+        }).showToast()
+    }
+    
+
+}
+
   return (
     <div className='mt-4 grow flex items-center justify-around'>
         <div className='mb-32'>
             <h1 className='text-4xl text-center mb-4'>Register</h1>
-            <form className='max-w-md mx-auto'>
+            <form className='max-w-md mx-auto' onSubmit={registerUser}>
                 <input 
                     type={'text'} 
                     placeholder={'Name'} 
