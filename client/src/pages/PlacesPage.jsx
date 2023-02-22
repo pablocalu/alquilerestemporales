1731be68175beb24d0f48ec502aeb20fe94f4e76
Pablo Carro <pablo.carro@live.com>
2023-02-22 02:11:24 -0300

    form

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,38 +1,185 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom'
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 export default function PlacesPage() {
-
-  const { action } = useParams()
+  const { action } = useParams();
 
   return (
     <div>
-      {
-        action !== 'new' && (
-          <div className='text-center'>
-          <Link className='inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full ' to={'/account/places/new'}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-              </svg>
-              Add new place
+      {action !== 'new' && (
+        <div className="text-center">
+          <Link
+            className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full "
+            to={'/account/places/new'}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+            Add new place
           </Link>
         </div>
-        )
-      }
-      {
-        action === 'new' && (
-          <div>
-            <form>
-              <h2 className='text-xl mt-4'>Title</h2>
-              <p className='text-gray-500'>Title should be short and attractive</p>
-              <input type="text" placeholder='title'/>
-              <h2>Address</h2>
-              <input type="text" placeholder='address' />
-              <h2>Photos</h2>
-            </form>
-          </div>
-        )
-      }
+      )}
+      {action === 'new' && (
+        <div>
+          <form>
+            <h2 className="text-xl mt-4">Title</h2>
+            <p className="text-gray-500 text-sm">
+              Title should be short and attractive
+            </p>
+            <input type="text mt-4" placeholder="title" />
+            <h2 className="text-xl mt-4">Address</h2>
+            <p className="text-gray-500 text-sm">This is the place for your address</p>
+            <input type="text mt-4" placeholder="address" />
+            <h2 className="text-xl mt-4">Photos</h2>
+            <p className="text-gray-500 text-sm">Put some photos</p>
+            <div className="flex gap-2">
+              <input type="text" placeholder="Add using a link" />
+              <button className="bg-gray-200 px-4 rounded-2xl">
+                Add&nbsp;photo
+              </button>
+            </div>
+            <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+              <button className="flex gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                  />
+                </svg>
+                Upload
+              </button>
+            </div>
+            <h2 className="text-xl mt-4">Description</h2>
+            <p className="text-gray-500 text-sm">Put some fancy description</p>
+            <textarea />
+            <h2 className="text-xl mt-4">Perks</h2>
+            <p className="text-gray-500 text-sm">Select all the perks you have</p>
+            <div className='grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6'>
+              <label className='border p-4 flex rounded-2xl gap-2 items-center cursos-pointer'>
+                <input type="checkbox" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z"
+                  />
+                </svg>
+                <span>Wi Fi</span>
+              </label>
+              <label className='border p-4 flex rounded-2xl gap-2 items-center cursos-pointer'>
+                <input type="checkbox" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z"
+                  />
+                </svg>
+
+                <span>Spotify</span>
+              </label>
+              <label className='border p-4 flex rounded-2xl gap-2 items-center cursos-pointer'>
+                <input type="checkbox" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z"
+                  />
+                </svg>
+
+                <span>TV</span>
+              </label >
+              <label className='border p-4 flex rounded-2xl gap-2 items-center cursos-pointer'>
+                <input type="checkbox" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
+                  />
+                </svg>
+
+                <span>Free Parking</span>
+              </label>
+              <label className='border p-4 flex rounded-2xl gap-2 items-center cursos-pointer'>
+                <input type="checkbox" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+
+                <span>All currencys</span>
+              </label>
+            </div>
+            <h2 className="text-2xl mt-4">Extra Info</h2>
+            <p className="text-gray-500 text-sm">Put some other info</p>
+            <textarea/>
+            <h2 className="text-2xl mt-4">Check in&out, max guests</h2>
+            <p className="text-gray-500 text-sm">Select an available date</p>
+            <div>
+              
+            </div>
+          </form>
+        </div>
+      )}
     </div>
-  )
+  );
 }
