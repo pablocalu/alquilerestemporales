9b868a333de445a9bc68aa717cda30746b29af46
Pablo Carro <pablo.carro@live.com>
2023-02-26 21:42:13 -0300

    center

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -57,7 +57,7 @@ export default function PlacePage() {
   }
 
   return (
-    <div className="mt-8 bg-gray-100 -mx-8 px-8 py-8">
+    <div className="mt-8 bg-gray-100 -mx-8 px-8 pt-8 max-w-4xl self-center">
       <h1 className="text-3xl">{place.title}</h1>
       <a
         className="flex gap-1 my-3 font-semibold underline"
@@ -83,7 +83,6 @@ export default function PlacePage() {
             d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
           />
         </svg>
-
         {place.address}
       </a>
       <div className="relative">
@@ -139,8 +138,7 @@ export default function PlacePage() {
           Show more photos
         </button>
       </div>
-
-      <div className="mt-8 gap-8 grid grid-cols-1 md:grid-cols-[2fr_1fr]">
+      <div className="mt-8 mb-8 gap-8 grid grid-cols-1 md:grid-cols-[2fr_1fr]">
         <div>
           <div className="py-4">
             <h2 className="font-semibold text-2xl">Description</h2>
@@ -154,6 +152,11 @@ export default function PlacePage() {
           <BookingWidget place={place}/>
         </div>
       </div>
+      <div className="bg-white -mx-8 px-8  border-t"></div>
+      <div>
+        <h2 className='font-semibold text-2xl'>Extra info</h2>
+      </div>
+      <div className='mb-4 mt-2 text-sm text-gray-700 leading-4'>{place.extraInfo}</div>
     </div>
   );
 }
