83e9b165cbd036ef0ae5f973727a64d4998000b5
Pablo Carro <pablo.carro@live.com>
2023-03-14 19:40:11 -0300

    changes

--- client/src/components/BookingWidget.jsx
+++ client/src/components/BookingWidget.jsx
@@ -1,5 +1,4 @@
-import React, { useContext, useEffect, useState, useRef } from 'react';
-import { addDays, differenceInCalendarDays } from 'date-fns';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios'
 import { UserContext } from '../UserContext';
 import { Navigate } from 'react-router-dom';
